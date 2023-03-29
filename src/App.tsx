1ffbb8ea2af173acee8ea5f0b28e9960c0d50c4d
Dan <danielmhutchings@gmail.com>
2023-03-29 13:30:25 +0200

    update hero title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div>
       <Header />
-      <Hero title='Where the wasteland ends' link={{ href: "#albums", children: "listen" }} />
+      {/* <Hero title='Where the wasteland ends' link={{ href: "#albums", children: "listen" }} /> */}
+      <Hero title='Next&nbsp;show 29/04/23 Berlin' link={{ href: "https://gseggmbh.de/hauser/rockhaus/", target: "_blank", children: "more" }} />
     </div>
   )
 }
