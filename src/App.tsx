51a5a45dd5a2433a197fdf03bfd4a4dd8a62aefd
Dan <danielmhutchings@gmail.com>
2023-03-29 23:21:44 +0200

    add news section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Header from './components/Header';
 import Hero from './components/Hero'
+import News from './components/News'
 
 function App() {
   return (
     <div>
       <Header />
       {/* <Hero title='Where the wasteland ends' link={{ href: "#albums", children: "listen" }} /> */}
-      <Hero title='Next&nbsp;show 29/04/23 Berlin' link={{ href: "https://gseggmbh.de/hauser/rockhaus/", target: "_blank", children: "more" }} />
+      <Hero title='Next&nbsp;show 29/04/23 Berlin' link={{ href: "#news", target: "_self", children: "more" }} />
+      <News anchor="news" title="News" description="Next live show will be at the Rockhaus in Berlin, Buchberger Straße 6, 10365, on April 29th, 2023, doors at 8pm. First on the bill, followed by Silky Psyche, and Inspectre" link={{ href: "https://www.berlin.de/tickets/suche/orte/rockhaus-berlin-0347ac7e-efc2-46d8-9022-16ff21964ed3/", target: "_blank", children: "Location" }} />
     </div>
   )
 }
