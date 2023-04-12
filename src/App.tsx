a21002b386f498ea0ae2b66edb7ff1eb1aca2c70
Dan <danielmhutchings@gmail.com>
2023-04-12 23:38:30 +0100

    update facebook event link

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         <Video youtube="Wi9ggsd0Lsw" />
       </section>
       <section ref={liveRef}>
-        <News title="News" description="Next live show will be at the Rockhaus in Berlin, Buchberger Straße 6, 10365, on April 29th, 2023, doors at 8pm. First on the bill, followed by Silky Psyche, and Inspectre" link={{ href: "https://www.berlin.de/tickets/suche/orte/rockhaus-berlin-0347ac7e-efc2-46d8-9022-16ff21964ed3/", target: "_blank", children: "Location" }} />
+        <News title="News" description="Next live show will be at the Rockhaus in Berlin, Buchberger Straße 6, 10365, on April 29th, 2023, doors at 8pm. First on the bill, followed by Silky Psyche, and Inspectre" link={{ href: "https://fb.me/e/2tW0FW1n3", target: "_blank", children: "Event" }} />
       </section>
       <section ref={buyRef}>
         <News title="Buy" description="...and listen" link={{ href: "https://marmadukedando.bandcamp.com/", target: "_blank", children: "Bandcamp" }} />
